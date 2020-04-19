5c9820e5cd63b16c06fed34f063db4d98d58757c
Zethson <lukas.heumos@posteo.net>
2020-04-19 11:54:06 +0200

    [FEATURE] github support with tokens

--- qube/create/github_support.py
+++ qube/create/github_support.py
@@ -1,10 +1,12 @@
 import click
 import os
 
+from cryptography.fernet import Fernet
 from distutils.dir_util import copy_tree
 from subprocess import Popen, PIPE
 from github import Github, GithubException
 from git import Repo
+from pathlib import Path
 
 
 def create_push_github_repository(project_name: str, project_description: str, template_creation_path: str) -> None:
@@ -22,14 +24,8 @@ def create_push_github_repository(project_name: str, project_description: str, t
     # Prompt for Github username, organization name if wished and whether private repository
     github_username: str = click.prompt('Please enter your Github account username: ',
                                         type=str)
-
-    click.echo(click.style('Please navigate to Github -> Your profile -> Developer Settings -> Personal access token -> Generate a new Token', fg='blue'))
-    click.echo(click.style('Please only tick \'repo\'. Note that the token is a hidden input to QUBE.', fg='blue'))
-    click.echo(click.style('For more information please read'
-                           ' https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line', fg='blue'))
-    access_token: str = click.prompt('Please enter your GitHub access token: ',
-                                     type=str,
-                                     hide_input=True)
+    # the personal access token for GitHub
+    access_token = handle_pat_authentification()
 
     is_github_org: bool = click.prompt('Do you want to create an organization repository? [y, n]',
                                        type=bool,
@@ -56,7 +52,7 @@ def create_push_github_repository(project_name: str, project_description: str, t
         repo = user.create_repo(project_name, description=project_description, private=private)
 
     click.echo(click.style('Creating labels and default Github settings.', fg='blue'))
-    create_dependabot_label(repo=repo, name="DEPENDABOT")
+    create_github_label(repo=repo, label="DEPENDABOT")
 
     repository = f'{os.getcwd()}/{project_name}'
 
@@ -91,6 +87,67 @@ def create_push_github_repository(project_name: str, project_description: str, t
     click.echo(click.style(f'Successfully created a Github repository at https://github.com/{github_username}/{project_name}', fg='green'))
 
 
+def handle_pat_authentification() -> str:
+    """
+    Try to read the encrypted Personal Access Token for GitHub.
+    If this fails (maybe there was no generated key before) then encrypt and return the PAT afterwards.
+    :return: The decrypted PAT
+    """
+
+    # check if the key and encrypted PAT already exist
+    if os.path.exists(f'{Path.home()}/.ct_keys') and os.path.exists(f'{Path.home()}/qube_conf'):
+        pat = decrypt_pat()
+
+        return pat
+
+    else:
+        click.echo(click.style('Could not read key from ~/qube_conf!\n', fg='red'))
+        click.echo(click.style('Please navigate to Github -> Your profile -> Developer Settings -> Personal access token -> Generate a new Token', fg='blue'))
+        click.echo(click.style('Please only tick \'repo\'. Note that the token is a hidden input to QUBE.', fg='blue'))
+        click.echo(click.style('For more information please read'
+                               ' https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line', fg='blue'))
+        access_token = click.prompt('Please enter your GitHub access token: ',
+                                    type=str,
+                                    hide_input=True)
+        access_token_b = access_token.encode('utf-8')
+
+        # encrypt the given PAT and save the encryption key and encrypted PAT in separate files
+        click.echo(click.style('Encrypting personal access token', fg='blue'))
+        key = Fernet.generate_key()
+        fer = Fernet(key)
+        encrypted_pat = fer.encrypt(access_token_b)
+
+        with open(f'{Path.home()}/qube_conf', 'wb') as f:
+            f.write(encrypted_pat)
+
+        with open(f'{Path.home()}/.ct_keys', 'wb') as f:
+            f.write(key)
+
+        pat = decrypt_pat()
+        return pat
+
+
+def decrypt_pat() -> str:
+    """
+    Decrypt the encrypted PAT.
+    :return: The decrypted Personal Access Token for GitHub
+    """
+
+    # read key and encrypted PAT from files
+    with open(f'{Path.home()}/.ct_keys', 'rb') as f:
+        key = f.readline()
+
+    with open(f'{Path.home()}/qube_conf', 'rb') as f:
+        encrypted_pat = f.readline()
+
+    # decrypt the PAT and decode it to string
+    click.echo(click.style('Decrypting personal access token', fg='blue'))
+    fer = Fernet(key)
+    decrypted_pat = fer.decrypt(encrypted_pat).decode('utf-8')
+
+    return decrypted_pat
+
+
 def is_git_accessible() -> bool:
     """
     Verifies that git is accessible and in the PATH.
@@ -107,14 +164,13 @@ def is_git_accessible() -> bool:
     return True
 
 
-def create_dependabot_label(repo, name: str) -> None:
+def create_github_label(repo, label: str) -> None:
     """
-    Create a dependabot label and add it to the repository.
-    If failed, print error message.
+    Create a Github label and add it to the repository.
     :param repo: The repository where the label needs to be added
-    :param name: The name of the new label
+    :param label: The name of the new label
     """
     try:
-        repo.create_label(name=name, color="1BB0CE")
+        repo.create_label(name=label, color="1BB0CE")
     except GithubException:
-        click.echo(click.style("Unable to create label {} due to permissions".format(name), fg='red'))
+        click.echo(click.style(f'Unable to create label {label} due to permissions', fg='red'))
