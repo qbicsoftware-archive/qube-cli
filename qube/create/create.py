f8f52e92cf92b725fa2879b169a5edc1883da20e
Zethson <lukas.heumos@posteo.net>
2020-04-23 17:11:24 +0200

    [FEATURE] portlet-groovy

--- qube/create/create.py
+++ qube/create/create.py
@@ -1,6 +1,7 @@
 import click
 
 from qube.create.domains.GuiCreator import GuiCreator
+from qube.create.domains.PortletCreator import PortletCreator
 from qube.create.domains.ServiceCreator import ServiceCreator
 from qube.create.domains.LibCreator import LibCreator
 from qube.create.domains.CliCreator import CliCreator
@@ -9,21 +10,22 @@ from qube.create.domains.CliCreator import CliCreator
 def choose_domain(domain: str):
     """
     Prompts the user for the template domain.
-    Creates the .cookietemple file.
     Prompts the user whether or not to create a Github repository
+
     :param domain: Template domain
     """
 
     if not domain:
         domain = click.prompt('Choose between the following domains ',
-                              type=click.Choice(['cli', 'lib', 'gui', 'service']),
+                              type=click.Choice(['cli', 'lib', 'gui', 'service', 'portlet']),
                               show_choices=True)
 
     switcher = {
         'cli': CliCreator,
         'lib': LibCreator,
         'gui': GuiCreator,
-        'service': ServiceCreator
+        'service': ServiceCreator,
+        'portlet': PortletCreator
     }
 
     creator_obj = switcher.get(domain.lower(), lambda: 'Invalid domain!')()
--- qube/lint/lint.py
+++ qube/lint/lint.py
@@ -8,6 +8,7 @@ from qube.lint.TemplateLinter import TemplateLinter
 from qube.lint.domains.cli import CliJavaLint
 from qube.lint.domains.gui import GuiJavaLint
 from qube.lint.domains.lib import LibJavaLint
+from qube.lint.domains.portlet import PortletGroovyLint
 from qube.lint.domains.service import ServiceJavaLint
 
 
@@ -22,7 +23,8 @@ def lint_project(project_dir: str) -> TemplateLinter:
         'cli-java': CliJavaLint,
         'lib-java': LibJavaLint,
         'gui-java': GuiJavaLint,
-        'service-java': ServiceJavaLint
+        'service-java': ServiceJavaLint,
+        'portlet-groovy': PortletGroovyLint
     }
 
     lint_obj = switcher.get(template_handle, lambda: 'Invalid')(project_dir)
@@ -57,7 +59,7 @@ def lint_project(project_dir: str) -> TemplateLinter:
 
 def get_template_handle(dot_qube_path: str = '.qube.yml') -> str:
     """
-    Reads the .cookietemple file and extracts the template handle
+    Reads the .qube file and extracts the template handle
     :param dot_qube_path: path to the .qube.yml file
     :return: found template handle
     """
