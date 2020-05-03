bf6dd1d1818b71a217c6af4fe999bf87c434e85f
Zethson <lukas.heumos@posteo.net>
2020-05-03 22:44:32 +0200

    Bump version from 0.1.0 to 1.1.0

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/qbicsoftware/qube',
-    version='0.1.0',
+    version='1.1.0',
     zip_safe=False,
 )
