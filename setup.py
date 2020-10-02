8081c836637597df7f230705e98c1158a4f5b565
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-10-02 10:07:44 +0200

    Bump version from 2.4.4 to 2.4.5

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/qbicsoftware/qube',
-    version='2.4.4',
+    version='2.4.5',
     zip_safe=False,
 )
