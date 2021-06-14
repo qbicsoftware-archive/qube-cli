43c4f2d5ebdeea4c421b95e9905d4f31c0ceff94
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-06-14 08:59:52 +0200

    Account for multi module maven project (#326)
    Adapt the groovy portlet so that it creates a multi-module maven project 
    
    Co-authored-by: Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

---
+++ qube/create/templates/portlet/portlet_groovy/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}-domain/src/main/groovy/life.qbic.business/Dummy.groovy
@@ -0,0 +1,11 @@
+package life.qbic.business
+/**
+ * <h1>This serves as a dummy class to make the project run</h1>
+ *
+ * <p>feel free to replace this class</p>
+ *
+ * @since 0.1.0-SNAPSHOT
+ *
+ */
+class Dummy {
+}
